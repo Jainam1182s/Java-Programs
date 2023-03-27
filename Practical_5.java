8dc9da3e924d29445e4a767754a01bb8a3d66ede
Jainam Priteshkumar Shah <jainam2711s@gmail.com>
2023-03-27 22:07:51 +0530

    Add files via upload

---
+++ Practical_5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Practical_5 {
+    static void maxNo(int a, int b, int c) {
+        if(a > b) {
+            if(a > c) {
+                System.out.println(a + " is Maximum.");
+            }
+            else{
+                System.out.println(c + " is Maximum.");
+            }
+        }
+        else if(a < b) {
+            if(b > c) {
+                System.out.println(b + " is Maximum.");
+            }
+            else {
+                System.out.println(c + " is Maximum.");
+            }
+        }
+        else{
+            System.out.println("All are Same.");
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Three Numbers := ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        maxNo(a,b,c);
+    }
+}
