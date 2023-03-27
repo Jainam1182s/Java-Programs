8890284b070e8766a790b4c50e5a5126f2b72402
Jainam Priteshkumar Shah <jainam2711s@gmail.com>
2023-03-27 22:07:10 +0530

    Add files via upload

---
+++ Practical_3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Practical_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number in METERS:= ");
+        float meter = sc.nextFloat();
+        float feet = meter * 3.28084f;
+        System.out.println("Value of Feet:= " + feet);
+    }
+}
