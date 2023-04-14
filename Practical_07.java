d47460e93ab30b7dc6cc72f32de1a3e8096f42f8
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-04-15 00:25:04 +0530

    Update Practical_07.java

--- Practical_07.java
+++ Practical_07.java
@@ -7,7 +7,6 @@ public class Practical_07 {
         String str = sc.nextLine();
         int length = str.length();
         System.out.println();
-        //System.out.println("The size of " + "the String is " + length);
         System.out.println("Second-Half of the String is := ");
         System.out.println(str.substring(length/2,length));
     }
