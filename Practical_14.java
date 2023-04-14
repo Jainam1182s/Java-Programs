ebb515811caa6f380f363352cea911cd6bd87a71
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-04-15 00:28:55 +0530

    Update Practical_14.java

--- Practical_14.java
+++ Practical_14.java
@@ -5,7 +5,6 @@ public class Practical_14 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter an Integer := ");
         int a = sc.nextInt();
-        //int b = a;
         int div = 2;
         System.out.print("1");
         while (a > 1) {
