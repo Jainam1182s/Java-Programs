94fd8fe0fab073ccf497862315ea504663b9c17f
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-04-15 00:30:53 +0530

    Update Practical_23.java

--- Practical_23.java
+++ Practical_23.java
@@ -16,9 +16,6 @@ public class Practical_23 {
         System.out.println("Enter Radius in Double := ");
         double r1 = sc.nextDouble();
         Practical_23 p1 = new Practical_23(r1);
-//        System.out.println("Enter Radius in Integer := ");
-//        int r2 = sc.nextInt();
-//        int r2 = (int)r1;
         Practical_23 p2 = new Practical_23((int)r1);
         System.out.println("Area of the circle is := " + p1.Area);
         System.out.println("Area of the circle is := " + p2.Area);
