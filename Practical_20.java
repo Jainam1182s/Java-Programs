9fb6db6785f1f09fbabf198dac13f988900be5f6
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-04-14 22:42:31 +0530

    Add files via upload

---
+++ Practical_20.java
@@ -0,0 +1,114 @@
+// =:DOUBT:=
+
+import java.util.Scanner;
+
+//import static java.lang.System.exit;
+
+public class Practical_20 {
+    int[] arr;
+    int top;
+    int capacity;
+
+    Practical_20(int size) {
+        arr = new int[size];
+        capacity = size;
+        top = -1;
+    }
+
+    void push(int x) {
+        if (isFull()) {
+            System.out.println("Stack is Full...!!");
+            System.exit(1);
+        }
+        arr[++top] = x;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            System.out.println(arr[top--]);
+        }
+        else {
+            System.out.println("Stack is Empty...!!");
+            System.exit(1);
+        }
+
+    }
+
+    void peek() {
+        if (isEmpty()) {
+            System.out.println(arr[top]);
+        }
+        else {
+            System.out.println("Stack is Empty...!!");
+            System.exit(1);
+        }
+
+    }
+
+    public void print() {
+        if (top == -1) {
+            System.out.println("Stack is Empty...!!");
+        } else {
+            System.out.println("Stack elements := ");
+            for (int i = top; i >= 0; i--) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+    boolean isEmpty() {
+        return top != -1;
+    }
+
+    boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of Elements := ");
+        int n = sc.nextInt();
+
+        Practical_20 pr = new Practical_20(n);
+
+        int ele;
+        System.out.println("Enter Elements := ");
+        for(int i=0;i<n;i++)
+        {
+            ele = sc.nextInt();
+            pr.push(ele);
+        }
+
+        outer:
+        while (pr.top >= -1) {
+            System.out.println("1.DISPLAY");
+            System.out.println("2.POP");
+            System.out.println("3.PEEK");
+            System.out.println("4.EXIT");
+            int choice = sc.nextInt();
+            System.out.println();
+
+            switch (choice) {
+                case 1 :
+                    pr.print();
+                    break;
+
+                case 2 :
+                    pr.pop();
+                    System.out.println();
+                    break;
+
+                case 3:
+                    pr.peek();
+                    System.out.println();
+                    break;
+                case 4 :
+                    break outer;
+                    //exit(1);
+
+                default :
+                    System.out.println("Invalid");
+            }
+        }
+    }
+}
+
