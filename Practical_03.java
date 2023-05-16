ede8e224bd81b65e6d9dbe86711932ad2fdfc8bb
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-05-16 07:16:47 +0530

    Update Practical_03.java

--- Practical_03.java
+++ Practical_03.java
@@ -1,3 +1,6 @@
+/* Question := Write a program that reads a number in meters, converts it to feet, 
+               and displays the result.*/
+
 import java.util.Scanner;
 
 public class Practical_03 {
@@ -9,3 +12,8 @@ public class Practical_03 {
         System.out.println("Value of Feet:= " + feet);
     }
 }
+
+/* Output :=
+        Enter a number in METERS:= 25
+        Value of Feet:= 82.020996
+*/
