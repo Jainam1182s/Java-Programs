99a7612ab88dfb5df64b4368759af9f1795ea109
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-06-13 20:47:46 +0530

    Update Practical_14.java

--- Practical_14.java
+++ Practical_14.java
@@ -1,3 +1,7 @@
+/*Question := Write a program that reads an integer and displays all its smallest factors in 
+              increasing order. For example if input number is 120, the output should be as 
+              follows:2,2,2,3,5.*/
+
 import java.util.Scanner;
 
 public class Practical_14 {
@@ -19,3 +23,10 @@ public class Practical_14 {
         System.out.println(".");
     }
 }
+
+/*
+Output :=
+Enter an Integer := 
+360
+1, 2, 2, 2, 3, 3, 5.
+*/
