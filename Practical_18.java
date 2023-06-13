d8d4c8172d5fbdec654012afb6334fe7ea1ad899
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-06-13 21:11:35 +0530

    Update Practical_18.java

--- Practical_18.java
+++ Practical_18.java
@@ -1,5 +1,8 @@
 //This Program is run on terminal.
 
+/*Question := Write a program to sort the elements from given array of integer. Specify 
+              size of array and elements of array from command line argument.*/
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -18,3 +21,15 @@ public class Practical_18 {
         }
     }
 }
+
+/*
+Output :=
+java Practical_18 5 10 15 20 25 30
+Enter Number Of Elements: 5
+5
+10
+15
+20
+25
+
+*/
