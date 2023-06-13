af47ed0f586013d4c896e584b2e6c17fd72ddca5
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2023-06-13 21:34:20 +0530

    Update Practical_22.java

--- Practical_22.java
+++ Practical_22.java
@@ -1,3 +1,8 @@
+/*Question := Write a program of constructor overloading and make zero argument constructor 
+              to set a default values of student name, roll no and total marks. And make 
+              another constructor with all the three parameters and make use of ‘this’ 
+              keyword. Using methods, display values of both the constructors.*/
+
 import java.util.Scanner;
 
 public class Practical_22 {
@@ -23,8 +28,15 @@ public class Practical_22 {
         System.out.println(marks);
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Practical_22 p1 = new Practical_22();
+
+        System.out.println("Before Updating := ");
+        p1.display();
+
+        System.out.println("=======================================");
+
         System.out.println("For Update := ");
         System.out.println("Enter Name of Student :=");
         String name = sc.nextLine();
@@ -33,13 +45,7 @@ public class Practical_22 {
         System.out.println("Enter Total Marks :=");
         int marks = sc.nextInt();
 
-        Practical_22 p1 = new Practical_22();
         Practical_22 p2 = new Practical_22(name, enroll, marks);
-
-        System.out.println("=======================================");
-
-        System.out.println("Before Updating := ");
-        p1.display();
 
         System.out.println("=======================================");
 
@@ -48,3 +54,23 @@ public class Practical_22 {
 
     }
 }
+/*
+Output := 
+Before Updating := 
+Jainam
+2101201075
+488
+=======================================
+For Update := 
+Enter Name of Student :=
+John
+Enter Enrollment No. :=
+2101201205
+Enter Total Marks :=
+79
+=======================================
+After Updating := 
+John
+2101201205
+79
+*/
